f7f5b34c2a311b5f9aa169e4718c6d072c3d0703
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-09-23 08:54:36 +0200

    plusieurs champs

--- src/app.js
+++ src/app.js
@@ -195,26 +195,39 @@ class Home extends React.Component {
     constructor (props) {
         super(props)
         this.state = {
-            nom: 'demo2'
+            nom: '',
+            prenom: '',
+            newsletter: false
         }
         this.handleChange = this.handleChange.bind(this)
     }
 
     handleChange (e) {
+        const name = e.target.name
+        const type = e.target.type
+        const value = type === 'checkbox' ? e.target.checked : e.target.value
         this.setState({
-            nom: e.target.value
+            [name]: value
         })
     }
 
     render () {
+        console.log('render')
         return <div>
-            <label htmlFor="nom">Mon nom</label>
-            <select value={this.state.nom} onChange={this.handleChange}>
-                <option value="demo1">Demo 1</option>
-                <option value="demo2">Demo 2</option>
-                <option value="demo3">Demo 3</option>
-            </select>
-
+            <div>
+                <label htmlFor="nom">Nom</label>
+                <input type="text" value={this.state.nom} onChange={this.handleChange} id="nom" name="nom"/>
+            </div>
+            <div>
+                <label htmlFor="prenom">Prénom</label>
+                <input type="text" value={this.state.prenom} onChange={this.handleChange} id="prenom" name="prenom"/>
+            </div>
+            <div>
+                <label htmlFor="newsletter">S'abonner à la newsletter ?</label>
+                <input type="checkbox" value={this.state.newsletter} onChange={this.handleChange} id="newsletter" name="newsletter"/>
+            </div>
+            <input type="text" defaultValue="Salut"/>
+            {JSON.stringify(this.state)}
         </div>
     }
 }
